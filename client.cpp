497dab77ff8b92bbb8970ac5960600ae9bef9b68
XanderHayhoe <xanderhayhoe@gmail.com>
2024-04-24 16:38:37 -0400

    client code

--- client.cpp
+++ client.cpp
@@ -10,7 +10,21 @@ int main(){
     char BT_address[18] = "34:FE:77:9D:C1:95";
     char response_buffer[1024] = { 0 };
 
+    int s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM); 
 
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = 1; // may need to cast to uint_8
+    str2ba(BT_address, &addr.rc_bdaddr);
+
+    int status = connect(s, (struct sockaddr* )&addr, sizeof(addr)); 
+
+    if (status < 0) {
+        std::cout << "OOPSIE POOPSIE NO CONNECTION :3" << std::endl;
+        return -1;
+    }
+    std::string command; 
+
+    // begin CLI functionality
 
     return 0; 
 }
