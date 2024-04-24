561704d9c1315c5d4e54537da198e13ac21c2bdd
XanderHayhoe <xanderhayhoe@gmail.com>
2024-04-24 16:25:00 -0400

    init client

--- client.cpp
+++ client.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+int main(){
+    sockaddr_rc addr = { 0 };  
+    char BT_address[17] = "34:FE:77:9D:C1:95";
+    char response_buffer[1024] = { 0 };
+
+
+
+    return 0; 
+}
+
